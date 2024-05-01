68c438aa0cc10bf5f94d65601dc2b0318834c97d
coderun13 <snehalsingh.0407@gmail.com>
2024-05-01 16:02:50 +0530

    create review schema

--- models/review.js
+++ models/review.js
@@ -3,3 +3,16 @@ const { object } = require("mongoose/lib/utils");
 const { create } = require("./listing");
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min:1,
+        max:5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
