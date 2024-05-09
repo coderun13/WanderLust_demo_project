185a8f284d6bdcf65c5f73e3c7040d52c729827c
coderun13 <snehalsingh.0407@gmail.com>
2024-05-09 23:01:10 +0530

    require ejsmate

--- models/listing.js
+++ models/listing.js
@@ -8,11 +8,11 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    /*image: {
+    image: {
         type: String,
         default: "nothing",
-        set: (v)=> v=== ""? "default link": v,
-    },*/
+        set: (v)=> v=== ""? "https://plus.unsplash.com/premium_photo-1682889762731-375a6b22d794?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D": v,
+    },
     price: Number,
     location: String,
     country: String,
