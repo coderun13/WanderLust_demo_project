d3b9e7d0092ddd0aa345cf7ab45a5779f770d40c
coderun13 <snehalsingh.0407@gmail.com>
2024-05-12 21:35:05 +0530

    modify listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { object } = require("mongoose/lib/utils");
+const { object, buffer } = require("mongoose/lib/utils");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -10,8 +10,9 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        default: "nothing",
-        set: (v)=> v=== ""? "https://plus.unsplash.com/premium_photo-1682889762731-375a6b22d794?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D": v,
+        // default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v)=> v=== ""? "https://plus.unsplash.com/premium_photo-1682889762731-375a6b22d794?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
     },
     price: Number,
     location: String,
