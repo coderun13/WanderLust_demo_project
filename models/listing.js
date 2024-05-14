e9cea802c38e37c87141f8679b5277c462ce7277
coderun13 <snehalsingh.0407@gmail.com>
2024-05-14 19:59:38 +0530

    add mongoose post middleware

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { object, buffer } = require("mongoose/lib/utils");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -19,11 +20,20 @@ const listingSchema = new Schema({
     country: String,
     reviews: [
         {
-        type:Schema.Types.ObjectId,
-        ref: "Review"
-    }
-    ]
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+    },
+    ],
 });
+
+
+//post mongoose middleware
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing){
+    await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
+
 
 const listing = mongoose.model("Listing",listingSchema);
 module.exports = listing;
