ea5f9316b3247731aff8ea5741ccffac3f72bc8b
coderun13 <snehalsingh.0407@gmail.com>
2024-05-16 02:08:35 +0530

    modify user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose"); 
 
+//email
 const userSchema = new Schema({
     email:{
         type: String,
@@ -9,6 +10,7 @@ const userSchema = new Schema({
     }
 })
 
-User.plugin(passportLocalMongoose);
+User.plugin(passportLocalMongoose); 
+//This plugin add usernane ,hash and salt field to store the username,the hashed password and the salt value.
 
-module.export = mongoose.model("user",userSchema);
+module.exports = mongoose.model("user",userSchema);
