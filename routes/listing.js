0b2199d7c169a620d58523ed0a0b574a73adc44e
coderun13 <snehalsingh.0407@gmail.com>
2024-05-17 01:07:45 +0530

    modify show route

--- routes/listing.js
+++ routes/listing.js
@@ -39,55 +39,15 @@ router.get
     ("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+      req.flash("error","Listing you requested for doest not exist!");
+      res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
   })
 );
 
-  //Create Route
-  router.post
-    ("/",validatelisting, 
-    wrapAsync(async(req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  })
-);
 
-
-  //Edit Route
-router.get
-    ("/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  })
-);
-  
-  
-  //Update Route
-  router.put
-    ("/:id",validatelisting,
-     wrapAsync(async (req, res) => {
-    // if(!req.body.listing){
-    //   throw new ExpressError(400,"Send valid data for listing");
-    // }
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-
-  //Delete Route
-router.delete
-    ("/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-  })
-);
-  
 
  //error Handling
 
