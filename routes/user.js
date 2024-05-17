2d0c1d7e50c6f632608a6aacba8ff737b377c02d
coderun13 <snehalsingh.0407@gmail.com>
2024-05-17 18:02:16 +0530

    add logout route

--- routes/user.js
+++ routes/user.js
@@ -52,5 +52,15 @@ router.post(
   );
 
 
+  router.get("/logout", (req,res,next) => {
+    req.logout((err)=>{
+        if(err){
+          return next(err);
+        }
+        req.flash("success","You are logged out!");
+        res.redirect("/listings");
+    });
+  });
+
 
 module.exports = router;
