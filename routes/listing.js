cad02ccf3c99816af8c8833d22a478c8e8599431
coderun13 <snehalsingh.0407@gmail.com>
2024-05-18 00:48:58 +0530

    populate owner

--- routes/listing.js
+++ routes/listing.js
@@ -39,7 +39,7 @@ router.get
 router.get
     ("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
       req.flash("error","Listing you requested for doest not exist!");
       res.redirect("/listings");
