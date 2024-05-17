230331a524d010dcff7fabb84b0485d219d1ed34
coderun13 <snehalsingh.0407@gmail.com>
2024-05-17 14:03:43 +0530

    require modules

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const user = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+
+
+module.exports = router;
