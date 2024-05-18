f5877fd10022b39c08d84d40286f69c9112a31a7
coderun13 <snehalsingh.0407@gmail.com>
2024-05-18 17:35:33 +0530

    add listings

---
+++ controllers/listings.js
@@ -0,0 +1,67 @@
+const Listing = require("../models/listing");
+
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+};
+
+
+module.exports.renderNewForm = (req, res) => {     
+    res.render("listings/new.ejs");
+};
+
+
+module.exports.showListings = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+    .populate({path:"reviews",
+      populate:{
+        path:"author"
+      },
+    })
+    .populate("owner");
+    if(!listing){
+      req.flash("error","Listing you requested for doest not exist!");
+      res.redirect("/listings");
+    }
+    res.render("listings/show.ejs", { listing });
+};
+
+
+module.exports.createListing = async(req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success","New listing created!");
+    res.redirect("/listings");
+};
+
+
+module.exports.renderEditForm = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing){
+      req.flash("error","Listing you requested for doest not exist!");
+      res.redirect("/listings");
+    }
+    req.flash("success","Listing Edited!");
+    res.render("listings/edit.ejs", { listing });
+};
+
+
+module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success","Listing Updated!");
+    res.redirect(`/listings/${id}`);
+};
+
+
+module.exports.destroyListing = async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    req.flash("success","Listing Deleted!");
+    console.log(deletedListing);
+    res.redirect("/listings");
+};
