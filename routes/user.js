b06ad6f901da118eaa3c4d290db7fc8bad55e202
coderun13 <snehalsingh.0407@gmail.com>
2024-05-18 17:33:22 +0530

    modify user.js

--- routes/user.js
+++ routes/user.js
@@ -6,35 +6,17 @@ const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
 
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-});
+const UserController = require("../controllers/users.js");
 
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        let {username,email,password} = req.body;
-        const newUser = new user({email,username,password});
-        const registeredUser = await user.register(newUser,password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success","Welcome to WanderLust!");
-            res.redirect("/listings");
-        });
-    } catch(e) {
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    };
-   
-}));
+router.get("/signup",UserController.renderSignupForm);
 
 
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
+router.post("/signup",
+wrapAsync(UserController.signup));
+
+
+router.get("/login",UserController.renderLoginForm);
 
 
 router.post(
@@ -44,23 +26,11 @@ router.post(
       { failureRedirect: '/login', 
       failureFlash: true
    }),
-      async(req,res)=>{
-          req.flash("success","Welcome back to WanderLust!");
-          let redirectUrl = res.locals.redirectUrl || "/listings";
-          res.redirect(redirectUrl);
-      }
+   UserController.login
   );
 
 
-  router.get("/logout", (req,res,next) => {
-    req.logout((err)=>{
-        if(err){
-          return next(err);
-        }
-        req.flash("success","You are logged out!");
-        res.redirect("/listings");
-    });
-  });
+  router.get("/logout",UserController.logout);
 
 
 module.exports = router;
