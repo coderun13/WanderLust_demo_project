ab87b110c1a51b043122b7086d85feae82448892
coderun13 <snehalsingh.0407@gmail.com>
2024-05-18 17:33:50 +0530

    modify review.js

--- routes/review.js
+++ routes/review.js
@@ -2,56 +2,27 @@ const express = require("express");
 const router = express.Router({ mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("../schema.js");
 const Listing = require("../models/listing.js");
 const Reviews = require("../models/review.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+const { isRef } = require("joi");
 
-
-//format for review schema validation
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-      let errMsg = error.details.map((el)=> el.message).join(",");
-      throw new ExpressError(400, result.errMsg);
-    }else{
-      next();
-    }
-  };
-
+  const reviewController = require("../controllers/reviews.js");
   
    //post route
   router.post
-  ("/",validateReview, wrapAsync(async (req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Reviews (req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    req.flash("success","New Review created!");
-
-  res.redirect(`/listings/${listing._id}`); 
-   })
-  );
+    ("/",
+    isLoggedIn,
+    validateReview,
+    wrapAsync(reviewController.createReview));
 
 
     //Delete Route
     router.delete
     ("/:reviewId",
-      wrapAsync(async (req,res) => {
-       let { id, reviewId } = req.params;
- 
-       await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
- 
-       await Reviews.findByIdAndDelete(reviewId);
- 
-       req.flash("success","Review Deleted!");
- 
-      res.redirect(`/listings/${id}`); 
-      })
-     );
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(reviewController.destroyReview));
  
  
 module.exports = router;
