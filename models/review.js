38fc4b329d6bc3ffd7d0ef118ea315194cf082df
coderun13 <snehalsingh.0407@gmail.com>
2024-05-18 17:34:46 +0530

    modify review.js

--- models/review.js
+++ models/review.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { object } = require("mongoose/lib/utils");
 const { create } = require("./listing");
+const { ref } = require("joi");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
@@ -13,7 +14,11 @@ const reviewSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now()
-    }
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },
 });
 
 const Review = mongoose.model("Review",reviewSchema);
